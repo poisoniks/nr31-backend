a33e6a345334efe532ed6de8c38871d91d33d993
poisoniks <poisoniks@mail.com>
2026-02-18 11:28:37 +0200

    Remove wildcards

--- src/main/java/org/nr31/backend/model/User.java
+++ src/main/java/org/nr31/backend/model/User.java
@@ -1,6 +1,15 @@
 package org.nr31.backend.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
