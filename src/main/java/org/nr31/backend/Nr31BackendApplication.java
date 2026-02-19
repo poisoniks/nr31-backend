d4bd0d9126e23440bbbe3d3d9a00e277aeaae10c
poisoniks <poisoniks@mail.com>
2026-02-19 12:58:21 +0200

    Add token cleanup job

--- src/main/java/org/nr31/backend/Nr31BackendApplication.java
+++ src/main/java/org/nr31/backend/Nr31BackendApplication.java
@@ -3,11 +3,13 @@ package org.nr31.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class Nr31BackendApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/nr31/backend/repository/RefreshTokenRepository.java
+++ src/main/java/org/nr31/backend/repository/RefreshTokenRepository.java
@@ -4,9 +4,11 @@ import org.nr31.backend.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
+    int deleteAllByExpiryDateBefore(Instant now);
 }
