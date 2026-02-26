3f49617308eabffa6fd63f77678a80e546959717
poisoniks <poisoniks@mail.com>
2026-02-26 22:46:59 +0200

    Remove HelloController.java

--- src/main/java/org/nr31/backend/config/SecurityConfig.java
+++ src/main/java/org/nr31/backend/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**", "/api/v1/hello", "/swagger-ui/**", "/v3/api-docs/**")
+                        .requestMatchers("/api/v1/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
                         .permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session
