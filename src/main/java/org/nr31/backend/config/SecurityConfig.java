a3f7b8f3c428b2803973eb4fc1eb3f0084da95ea
poisoniks <poisoniks@mail.com>
2026-02-27 13:07:35 +0200

    Deny all requests until permitted

--- src/main/java/org/nr31/backend/config/SecurityConfig.java
+++ src/main/java/org/nr31/backend/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**", "/api/v1/calendar/**","/swagger-ui/**", "/v3/api-docs/**")
                         .permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().denyAll())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
