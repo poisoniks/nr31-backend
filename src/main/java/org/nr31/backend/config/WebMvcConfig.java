65c03515f53244727e803b4f2934f50095c975fd
poisoniks <poisoniks@mail.com>
2026-03-11 18:40:44 +0200

    Fix pageable data serialization issue

--- src/main/java/org/nr31/backend/config/WebMvcConfig.java
+++ src/main/java/org/nr31/backend/config/WebMvcConfig.java
@@ -3,10 +3,12 @@ package org.nr31.backend.config;
 import lombok.RequiredArgsConstructor;
 import org.nr31.backend.interceptor.FeatureSwitchInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
